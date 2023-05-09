866815046ad49de2ebddc40a72a1a5999ef5f1ae
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2023-05-09 21:22:32 +0700

    second

--- pages/components/Pricelist.js
+++ pages/components/Pricelist.js
@@ -1,7 +1,7 @@
 export default function Pricelist() {
     return (
         <div id="pricelist" className="pt-16 pb-8">
-            <div className="bg-white">
+            <div className="">
                 <div className="mx-auto max-w-7xl">
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Pricelist</h2>
