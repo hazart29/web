1cabb891b09d2215256d7fd4ccf896ddb369eadf
Misbakhul Munir <74493085+hazart29@users.noreply.github.com>
2023-05-09 21:08:09 +0700

    second

---
+++ pages/components/Feature.js
@@ -0,0 +1,14 @@
+export default function Feature() {
+    return (
+        <div id="feature" className='flex flex-col gap-12 jusify-between h-screen pt-28'>
+            <div className='flex flex-col'>
+                <p className='font-bold text-5xl'>Shine Photo</p>
+                <p>"Telling Story With Love"</p>
+            </div>
+            <div className='flex flex-col'>
+                <div className='font-semibold'>Wedding, Prewedding, Enggagement, etc.</div>
+                <p className=''>Documentation</p>
+            </div>
+        </div>
+    )
+}
---
+++ pages/components/Pricelist.js
@@ -0,0 +1,85 @@
+export default function Pricelist() {
+    return (
+        <div id="pricelist" className="pt-28 pb-8">
+            <div className="bg-white">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl lg:mx-0">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Pricelist</h2>
+                        <p className="mt-2 text-lg leading-8 text-gray-600">To Do Next.</p>
+                    </div>
+
+                    <div className="mx-auto grid mt-10 pt-4 max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+
+                        <div className="flex max-w-xl flex-col gap-4 items-start justify-between bg-gray-50 p-4 rounded-lg">
+                            <div className="">
+                                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                    <a href="#">
+                                        <span className="absolute inset-0"></span>
+                                        Sirius Package
+                                    </a>
+                                </h3>
+                                <div className="mt-5 text-sm leading-6 text-gray-600 list-disc">
+                                    <p>Photo, Video Chinematic, Full Video Documentation</p>
+                                    <p>1 Flashdisk (all photo edited)</p>
+                                    <p>1 Wedding Book Collase (20 Hal Hard Cover)</p>
+                                    <p>1 Chinematic Video</p>
+                                    <p>1 Full Video Documentation</p>
+                                    <p>1 Day 8 Hours</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center gap-x-4 text-xs">
+                                <a href="#" className="relative text-lg z-10 rounded-full bg-gray-100 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">IDR 6.500K</a>
+                            </div>
+                        </div>
+
+                        <div className="flex max-w-xl flex-col gap-4 items-start justify-between bg-gray-50 p-4 rounded-lg">
+                            <div className="">
+                                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                    <a href="#">
+                                        <span className="absolute inset-0"></span>
+                                        Sirius Package
+                                    </a>
+                                </h3>
+                                <div className="mt-5 text-sm leading-6 text-gray-600 list-disc">
+                                    <p>Photo, Video Chinematic, Full Video Documentation</p>
+                                    <p>1 Flashdisk (all photo edited)</p>
+                                    <p>1 Wedding Book Collase (20 Hal Hard Cover)</p>
+                                    <p>1 Chinematic Video</p>
+                                    <p>1 Full Video Documentation</p>
+                                    <p>1 Day 8 Hours</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center gap-x-4 text-xs">
+                                <a href="#" className="relative text-lg z-10 rounded-full bg-gray-100 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">IDR 6.500K</a>
+                            </div>
+                        </div>
+
+                        <div className="flex max-w-xl flex-col gap-4 items-start justify-between bg-gray-50 p-4 rounded-lg">
+                            <div className="">
+                                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                    <a href="#">
+                                        <span className="absolute inset-0"></span>
+                                        Sirius Package
+                                    </a>
+                                </h3>
+                                <div className="mt-5 text-sm leading-6 text-gray-600 list-disc">
+                                    <p>Photo, Video Chinematic, Full Video Documentation</p>
+                                    <p>1 Flashdisk (all photo edited)</p>
+                                    <p>1 Wedding Book Collase (20 Hal Hard Cover)</p>
+                                    <p>1 Chinematic Video</p>
+                                    <p>1 Full Video Documentation</p>
+                                    <p>1 Day 8 Hours</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center gap-x-4 text-xs">
+                                <a href="#" className="relative text-lg z-10 rounded-full bg-gray-100 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">IDR 6.500K</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+} 
